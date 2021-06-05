2b746badab2eabcc3851f0b387e4a89b2f42a193
JuYoung <ken44929@gmail.com>
2021-06-05 18:42:31 +0900

    ImageUpload 컴포넌트에 대한 스타일을 담당하는 파일 생성

---
+++ client/src/components/utils/ImageUploadStyle.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const DropzoneBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DropzoneContainer = styled.div`
+  width: 460px;
+  height: 150px;
+  border: 3px dashed lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const DropzoneTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ImagesContainer = styled.div`
+  box-sizing: border-box;
+  width: 350px;
+  margin: 0;
+  padding: 0;
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 14px;
+  list-style: none;
+  line-height: 1.5715;
+`;
+
+export const MenuImageContainer = styled.div`
+  width: 100%;
+  position: relative;
+  height: 66px;
+  padding: 8px;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+  margin-bottom: 5px;
+`;
+
+export const MenuImageWrapper = styled.div`
+  height: 100%;
+  padding: 0 4px;
+  display: flex;
+  align-items: center;
+  //  transition: background-color .3s;
+`;
+
+export const MenuImage = styled.img`
+  display: block;
+  width: 48px;
+  height: 48px;
+  overflow: hidden;
+`;
+
+export const MenuImageTitle = styled.span`
+  flex: auto;
+  color: #7cb305;
+  padding: 0 15px;
+`;
