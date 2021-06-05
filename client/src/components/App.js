d16ccc6ca7fbfed6baa9a1d3665edd3fba3f2555
JuYoung <ken44929@gmail.com>
2021-06-05 18:41:07 +0900

    Upload 관련 페이지들 라우팅 정리

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,11 +12,11 @@ import HostDetailPage from "./views/HostDetailPage/HostDetailPage";
 import ListYourKitchenContainer from "./views/ListYourKitchenPage/ListYourKitchenContainer";
 import Footer from "./views/Footer/Footer";
 import { Layout, Button } from "antd";
-import UploadKitchenPage from "./admin/UploadKitchenPage";
-import UploadHostPage from "./admin/UploadHostPage";
+import UploadKitchenContainer from "./views/UploadKitchenPage/UploadKitchenContainer";
+import UploadHostContainer from "./views/UploadHostPage/UploadHostContainer";
 import KitchenContainer from "./views/KitchenPage/KitchenContainer";
 import EventContainer from "./views/EventPage/EventContainer";
-import UploadEventPage from "./admin/UploadEventPage";
+import UploadEventContainer from "./views/UploadEventPage/UploadEventContainer";
 import EventDetailContainer from "./views/EventDetailPage/EventDetailContainer";
 import UserProfilePresenter from "./views/UserProfilePage/UserProfilePresenter";
 import MyTicketPage from "./views/MyTicketPage/MyTicketPage";
@@ -40,107 +40,100 @@ const style = {
 function App() {
   return (
     <Suspense fallback={<Button type="primary" size="small" loading></Button>}>
-      <div
-      // style={{ background: "geekblue" }}
+      <NavBar />
+
+      <Content
+        style={{
+          // backgroundColor: "rgb(250, 250, 250)",
+          display: "flex",
+          flexDirection: "column",
+        }}
       >
-        <NavBar />
-
-        <Content
-          style={{
-            // backgroundColor: "rgb(250, 250, 250)",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          {/* <Switch> 는 첫번째로 매칭되는 path 를 가진 컴포넌트를 렌더링 시킨다. 
+        {/* <Switch> 는 첫번째로 매칭되는 path 를 가진 컴포넌트를 렌더링 시킨다. 
           이것이 exact path 와 다른 점은 첫번째 매칭만 본다는 것이다. */}
-          <Switch>
-            <Route exact path="/" component={Auth(LandingPage, null)} />
-            <MainBox>
-              <Route exact path="/login" component={Auth(LoginPage, false)} />
-              <Route
-                exact
-                path="/register"
-                component={Auth(RegisterPage, false)}
-              />
-              <Route
-                exact
-                path="/hosts"
-                component={Auth(HostContainer, true)}
-              />
-              <Route
-                exact
-                path="/become-a-host"
-                component={Auth(BecomeAHostContainer, true)}
-              />
-              <Route
-                path="/hosts/:hostsId"
-                component={Auth(HostDetailPage, null)}
-              />
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <MainBox>
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route
+              exact
+              path="/register"
+              component={Auth(RegisterPage, false)}
+            />
+            <Route exact path="/hosts" component={Auth(HostContainer, true)} />
+            <Route
+              exact
+              path="/become-a-host"
+              component={Auth(BecomeAHostContainer, true)}
+            />
+            <Route
+              path="/hosts/:hostsId"
+              component={Auth(HostDetailPage, null)}
+            />
 
-              <Route
-                exact
-                path="/list-your-kitchen"
-                component={Auth(ListYourKitchenContainer, null)}
-              />
-              <Route
-                exact
-                path="/kitchens"
-                component={Auth(KitchenContainer, null)}
-              />
-              <Route
-                exact
-                path="/kitchens/:kitchensId"
-                component={Auth(KitchenDetailContainer, null)}
-              />
-              <Route
-                exact
-                path="/events"
-                component={Auth(EventContainer, null)}
-              />
+            <Route
+              exact
+              path="/list-your-kitchen"
+              component={Auth(ListYourKitchenContainer, null)}
+            />
+            <Route
+              exact
+              path="/kitchens"
+              component={Auth(KitchenContainer, null)}
+            />
+            <Route
+              exact
+              path="/kitchens/:kitchensId"
+              component={Auth(KitchenDetailContainer, null)}
+            />
+            <Route
+              exact
+              path="/events"
+              component={Auth(EventContainer, null)}
+            />
 
-              {/* My Page */}
-              <Route
-                exact
-                path="/users/:userName"
-                component={Auth(UserProfilePresenter, null)}
-              />
+            {/* My Page */}
+            <Route
+              exact
+              path="/users/:userName"
+              component={Auth(UserProfilePresenter, null)}
+            />
 
-              <Route
-                exact
-                path="/my-tickets"
-                component={Auth(MyTicketPage, null)}
-              />
+            <Route
+              exact
+              path="/my-tickets"
+              component={Auth(MyTicketPage, null)}
+            />
 
-              {/* Admin Page */}
-              <Route
-                exact
-                path="/events/:eventId"
-                component={Auth(EventDetailContainer, null)}
-              />
-              <Route
-                exact
-                path="/upload-kitchen"
-                component={Auth(UploadKitchenPage, true)}
-              />
-              <Route
-                exact
-                path="/upload-host"
-                component={Auth(UploadHostPage, true)}
-              />
-              <Route
-                exact
-                path="/upload-event"
-                component={Auth(UploadEventPage, true)}
-              />
-            </MainBox>
-          </Switch>
-        </Content>
+            {/* Admin Page */}
+            <Route
+              exact
+              path="/events/:eventId"
+              component={Auth(EventDetailContainer, null)}
+            />
+            <Route
+              exact
+              path="/upload-kitchen"
+              component={Auth(UploadKitchenContainer, true)}
+            />
+            <Route
+              exact
+              path="/upload-host"
+              component={Auth(UploadHostContainer, true)}
+            />
+            <Route
+              exact
+              path="/upload-event"
+              component={Auth(UploadEventContainer, true)}
+            />
+          </MainBox>
+        </Switch>
+      </Content>
 
-        <BackTop>
-          <div style={style}>UP</div>
-        </BackTop>
-      </div>
+      <BackTop>
+        <div style={style}>UP</div>
+      </BackTop>
+
       <Footer />
     </Suspense>
   );
