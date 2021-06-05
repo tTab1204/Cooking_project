0c65e3735fb7eb568948bdbfb81697d36c44be26
JuYoung <ken44929@gmail.com>
2021-06-05 18:51:24 +0900

    HOST_SERVER api 추가

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,6 +1,6 @@
 //SERVER
-export const USER_SERVER = "/api/users";
 export const LOCAL_SERVER = "http://localhost:5000/";
+export const USER_SERVER = "/api/users";
 export const EVENTS_SERVER = "/api/events";
 export const HOST_SERVER = "/api/hosts";
 export const FOLLOWER_SERVER = "/api/follow";
@@ -9,3 +9,4 @@ export const FOLLOWER_SERVER = "/api/follow";
 export const EVENTS_CLIENT = "/events";
 export const KITCHNES_CLIENT = "/kitchens";
 export const HOST_CLIENT = "/hosts";
+Y;
