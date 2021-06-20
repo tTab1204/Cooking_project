3cb21f2a336f465cafa773bfdc7b0e07eef0b806
JuYoung <ken44929@gmail.com>
2021-06-20 18:38:34 +0900

    Prettier 변경에 따른 single quote, print width: 120 적용

--- client/src/components/views/EventDetailPage/EventDetailPresenter.js
+++ client/src/components/views/EventDetailPage/EventDetailPresenter.js
@@ -1,16 +1,5 @@
-import React, { useState } from "react";
-import {
-  Row,
-  Col,
-  Card,
-  Typography,
-  Divider,
-  Descriptions,
-  Button,
-  Avatar,
-  Affix,
-  Select,
-} from "antd";
+import React, { useState } from 'react';
+import { Row, Col, Card, Typography, Divider, Descriptions, Button, Avatar, Affix, Select } from 'antd';
 import {
   BottomButtonBox,
   MainImgWrapper,
@@ -19,59 +8,55 @@ import {
   DescriptionFollowers,
   MenuImageStyle,
   AffixBox,
-} from "./EventDetailStyle";
-import "./EventDetailStyle.css";
-import { Link } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
-import { HOST_CLIENT, LOCAL_SERVER } from "../../Config";
+} from './EventDetailStyle';
+import './EventDetailStyle.css';
+import { Link } from 'react-router-dom';
+import { UserOutlined } from '@ant-design/icons';
+import { HOST_CLIENT, LOCAL_SERVER } from '../../Config';
 
-import ImageSlideModal from "../../utils/ImageSlideModal";
+import ImageSlideModal from '../../utils/ImageSlideModal';
 
 const { Title } = Typography;
 const { Option } = Select;
 
 // ---------------------------- CSS-in-JS --------------------------- //
 const colStyle = {
-  alignItems: "center",
-  display: "flex",
-  flexDirection: "column",
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
 };
 
 const cardBodyStyle = {
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
 };
 
 const mainImgStyle = {
-  objectFit: "cover",
-  height: "100%",
-  width: "100%",
-  position: "absolute",
-  top: "0px",
-  right: "0px",
+  objectFit: 'cover',
+  height: '100%',
+  width: '100%',
+  position: 'absolute',
+  top: '0px',
+  right: '0px',
 };
 
 const bottomButtonStyle = {
-  width: "70%",
-  display: "block",
-  transition: "width 0.2s ease-in-out 0s",
-  boxShadow: "0.2s ease-in-out 0s",
+  width: '70%',
+  display: 'block',
+  transition: 'width 0.2s ease-in-out 0s',
+  boxShadow: '0.2s ease-in-out 0s',
 };
 
 const imageStyle = {
-  minWidth: "auto",
-  width: "auto",
-  border: "none",
+  minWidth: 'auto',
+  width: 'auto',
+  border: 'none',
 };
 
 // ----------------------------------------------------------------------//
 
-function EventDetailPresenter({
-  DetailEvent,
-  addToCartHandler,
-  onQuantityChange,
-}) {
+function EventDetailPresenter({ DetailEvent, addToCartHandler, onQuantityChange }) {
   // Modal
   const [CurrentSlide, setCurrentSlide] = useState(0);
   const [ShowModal, setShowModal] = useState(false);
@@ -103,25 +88,24 @@ function EventDetailPresenter({
     }
   };
 
-  const { images, name, host, description, time, location, writer } =
-    DetailEvent;
+  const { images, name, host, description, time, location, writer } = DetailEvent;
 
   return (
     <>
       {images && writer && (
         <div>
-          <Row type="flex">
+          <Row type='flex'>
             <Col sm={24} md={14} style={colStyle}>
-              <div style={{ width: "100%" }}>
-                <Row type="flex">
+              <div style={{ width: '100%' }}>
+                <Row type='flex'>
                   <Col span={24}>
                     <Card
                       hoverable={true}
-                      bodyStyle={{ padding: "0" }}
+                      bodyStyle={{ padding: '0' }}
                       cover={
                         <MainImgWrapper>
                           <img
-                            alt="Event_main"
+                            alt='Event_main'
                             src={`${LOCAL_SERVER}${images[0]}`}
                             style={mainImgStyle}
                             onClick={() => handleModalOpen(images[0], 0)}
@@ -134,34 +118,32 @@ function EventDetailPresenter({
               </div>
             </Col>
             <Col sm={24} md={10}>
-              <Card style={{ height: "100%" }} bodyStyle={cardBodyStyle}>
+              <Card style={{ height: '100%' }} bodyStyle={cardBodyStyle}>
                 <Title level={3}>{name}</Title>
-                <Divider style={{ margin: "16px 0px" }} />
+                <Divider style={{ margin: '16px 0px' }} />
                 <Descriptions column={1} colon={false}>
                   {/* 나중에 리팩토링 - 컴포넌트로 묶어버리자. */}
                   {/* Host*/}
                   <Descriptions.Item
-                    label="Host"
+                    label='Host'
                     style={{
-                      display: "grid",
-                      gridTemplateColumns: "1fr 3fr",
+                      display: 'grid',
+                      gridTemplateColumns: '1fr 3fr',
                     }}
                   >
                     <DescriptionContentContainer>
                       <DescriptionContentWrapper>
-                        <Link to={`${HOST_CLIENT}/${host._id}`}>
-                          {host.name}
-                        </Link>
+                        <Link to={`${HOST_CLIENT}/${host._id}`}>{host.name}</Link>
                         <DescriptionFollowers>
                           <UserOutlined
                             style={{
-                              color: "var(--primary-color3)",
-                              marginRight: "4px",
+                              color: 'var(--primary-color3)',
+                              marginRight: '4px',
                             }}
-                          />{" "}
+                          />{' '}
                           <Typography
                             style={{
-                              whiteSpace: "nowrap",
+                              whiteSpace: 'nowrap',
                             }}
                           >
                             4.8 (150)
@@ -170,31 +152,29 @@ function EventDetailPresenter({
                       </DescriptionContentWrapper>
                       <Avatar
                         src={`${LOCAL_SERVER}${host.image}`}
-                        size="large"
-                        style={{ width: "40px", height: "40px" }}
+                        size='large'
+                        style={{ width: '40px', height: '40px' }}
                       />
                     </DescriptionContentContainer>
                   </Descriptions.Item>
                   {/* Time */}
                   <Descriptions.Item
-                    label="Time"
+                    label='Time'
                     style={{
-                      display: "grid",
-                      gridTemplateColumns: "1fr 3fr",
+                      display: 'grid',
+                      gridTemplateColumns: '1fr 3fr',
                     }}
                   >
                     <DescriptionContentContainer>
-                      <DescriptionContentWrapper>
-                        {DetailEvent.time}
-                      </DescriptionContentWrapper>
+                      <DescriptionContentWrapper>{DetailEvent.time}</DescriptionContentWrapper>
                     </DescriptionContentContainer>
                   </Descriptions.Item>
                   {/* Location */}
                   <Descriptions.Item
-                    label="Location"
+                    label='Location'
                     style={{
-                      display: "grid",
-                      gridTemplateColumns: "1fr 3fr",
+                      display: 'grid',
+                      gridTemplateColumns: '1fr 3fr',
                     }}
                   >
                     <DescriptionContentContainer>
@@ -202,8 +182,8 @@ function EventDetailPresenter({
                         {DetailEvent.location}
                         <br />
                         <a
-                          target="_blank"
-                          href="https://www.google.com/maps/place/NAVER/@37.3538874,127.1014005,17z/data=!3m1!4b1!4m5!3m4!1s0x357b59b81a8b3727:0xf4082515883c699a!8m2!3d37.3538874!4d127.1035892"
+                          target='_blank'
+                          href='https://www.google.com/maps/place/NAVER/@37.3538874,127.1014005,17z/data=!3m1!4b1!4m5!3m4!1s0x357b59b81a8b3727:0xf4082515883c699a!8m2!3d37.3538874!4d127.1035892'
                         >
                           View Map
                         </a>
@@ -212,9 +192,7 @@ function EventDetailPresenter({
                   </Descriptions.Item>
                 </Descriptions>
                 <Divider />
-                <Typography style={{ whiteSpace: "pre-wrap" }}>
-                  {DetailEvent.description}
-                </Typography>
+                <Typography style={{ whiteSpace: 'pre-wrap' }}>{DetailEvent.description}</Typography>
               </Card>
             </Col>
           </Row>
@@ -224,17 +202,17 @@ function EventDetailPresenter({
             <Title level={3}>Menu</Title>
             <Row gutter={[8, 8]}>
               {images.map((image, index) => (
-                <Col key={index} className="gutter-row" lg={6} md={12} sm={24}>
+                <Col key={index} className='gutter-row' lg={6} md={12} sm={24}>
                   <Card
                     hoverable={true}
-                    bodyStyle={{ padding: "0" }}
+                    bodyStyle={{ padding: '0' }}
                     style={imageStyle}
                     cover={
                       <MenuImageStyle
-                        alt="event-menu-images"
+                        alt='event-menu-images'
                         src={`${LOCAL_SERVER}${image}`}
                         onClick={() => handleModalOpen(image, index)}
-                        style={{ maxHeight: "160px" }}
+                        style={{ maxHeight: '160px' }}
                       />
                     }
                   ></Card>
@@ -244,29 +222,24 @@ function EventDetailPresenter({
           </div>
 
           <BottomButtonBox>
-            <Affix offsetBottom={0} style={{ height: "72px" }}>
+            <Affix offsetBottom={0} style={{ height: '72px' }}>
               <AffixBox>
-                <Button
-                  style={bottomButtonStyle}
-                  type="primary"
-                  size="large"
-                  onClick={addToCartHandler}
-                >
+                <Button style={bottomButtonStyle} type='primary' size='large' onClick={addToCartHandler}>
                   Add To Cart
                 </Button>
                 <Select
                   labelInValue
                   defaultActiveFirstOption={true}
-                  size="large"
-                  placeholder="1"
-                  style={{ width: "15%" }}
+                  size='large'
+                  placeholder='1'
+                  style={{ width: '15%' }}
                   onChange={onQuantityChange}
                 >
-                  <Option value="1">1</Option>
-                  <Option value="2">2</Option>
-                  <Option value="3">3</Option>
-                  <Option value="4">4</Option>
-                  <Option value="5">5</Option>
+                  <Option value='1'>1</Option>
+                  <Option value='2'>2</Option>
+                  <Option value='3'>3</Option>
+                  <Option value='4'>4</Option>
+                  <Option value='5'>5</Option>
                 </Select>
               </AffixBox>
             </Affix>
--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Menu, Badge } from "antd";
-// import LoginPage from "../../LoginPage/LoginPage";
-import axios from "axios";
-import { USER_SERVER } from "../../../Config";
-import { withRouter, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { UserOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from 'react';
+import { Menu, Badge } from 'antd';
+import axios from 'axios';
+import { USER_SERVER } from '../../../Config';
+import { withRouter, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { UserOutlined } from '@ant-design/icons';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 function RightMenu(props) {
   const user = useSelector((state) => state.user.userData);
-  const cart = useSelector((state) => state.user.cart);
+  const cart = useSelector((state) => state.user.cartDetail);
+
+  let total = 0;
+  if (cart) cart.map((item) => (total += item.quantity));
 
   const logoutHandler = () => {
     axios.get(`${USER_SERVER}/logout`).then((response) => {
       if (response.status === 200) {
-        props.history.push("/login");
+        props.history.push('/login');
       } else {
-        alert("Log Out Failed");
+        alert('Log Out Failed');
       }
     });
   };
@@ -28,11 +30,11 @@ function RightMenu(props) {
     return (
       <>
         <Menu mode={props.mode}>
-          <Menu.Item key="mail">
-            <Link to="/login">Signin</Link>
+          <Menu.Item key='mail'>
+            <Link to='/login'>Signin</Link>
           </Menu.Item>
-          <Menu.Item key="app">
-            <Link to="/register">Signup</Link>
+          <Menu.Item key='app'>
+            <Link to='/register'>Signup</Link>
           </Menu.Item>
         </Menu>
       </>
@@ -42,75 +44,75 @@ function RightMenu(props) {
       <>
         {user && (
           <Menu
-            style={{ fontSize: "14px", borderBottom: "none" }}
+            style={{ fontSize: '14px', borderBottom: 'none' }}
             mode={props.mode}
           >
-            <Menu.Item key="app" style={{ borderBottom: "none" }}>
-              <Link to="/events">Events</Link>
+            <Menu.Item key='app' style={{ borderBottom: 'none' }}>
+              <Link to='/events'>Events</Link>
             </Menu.Item>
 
             <SubMenu
               title={<span>Kitchens</span>}
-              style={{ borderBottom: "none" }}
+              style={{ borderBottom: 'none' }}
             >
               <MenuItemGroup>
-                <Menu.Item key="kitchens">
-                  <Link to="/kitchens">Kitchen</Link>
+                <Menu.Item key='kitchens'>
+                  <Link to='/kitchens'>Kitchen</Link>
                 </Menu.Item>
-                <Menu.Item key="list-your-kitchen">
-                  <Link to="/list-your-kitchen">List Your Kitchen</Link>
+                <Menu.Item key='list-your-kitchen'>
+                  <Link to='/list-your-kitchen'>List Your Kitchen</Link>
                 </Menu.Item>
               </MenuItemGroup>
             </SubMenu>
             <SubMenu
               title={<span>Hosts</span>}
-              style={{ borderBottom: "none" }}
+              style={{ borderBottom: 'none' }}
             >
               <MenuItemGroup>
-                <Menu.Item key="hosts">
-                  <Link to="/hosts">Hosts</Link>
+                <Menu.Item key='hosts'>
+                  <Link to='/hosts'>Hosts</Link>
                 </Menu.Item>
-                <Menu.Item key="become-a-host">
-                  <Link to="/become-a-host">Become a Host</Link>
+                <Menu.Item key='become-a-host'>
+                  <Link to='/become-a-host'>Become a Host</Link>
                   Become a Host
                 </Menu.Item>
               </MenuItemGroup>
             </SubMenu>
             <SubMenu
-              style={{ borderBottom: "none" }}
+              style={{ borderBottom: 'none' }}
               title={
                 <span>
                   <Badge
                     overflowCount={10}
                     offset={[0, 2]}
-                    count={user.cart.length}
+                    count={cart && total}
                   >
-                    <UserOutlined style={{ fontSize: "20px" }} />
+                    <UserOutlined style={{ fontSize: '20px' }} />
                   </Badge>
                 </span>
               }
             >
               <MenuItemGroup>
-                <Menu.Item key="upload-event">
-                  <Link to="/upload-event">Upload Event</Link>
+                <Menu.Item key='upload-event'>
+                  <Link to='/upload-event'>Upload Event</Link>
                 </Menu.Item>
 
-                <Menu.Item key="my-profile">
+                <Menu.Item key='my-profile'>
                   <Link to={`/users/${user.name}`}>My Profile</Link>
                 </Menu.Item>
-                <Menu.Item key="my-tickets">
+                <Menu.Item key='my-tickets'>
                   <Badge
-                    count={user.cart.length}
+                    count={cart && total}
                     overflowCount={10}
                     offset={[12, 0]}
                   >
-                    <Link to="/my-tickets">My Tickets</Link>
+                    <Link to='/my-tickets'>My Tickets</Link>
                   </Badge>
                 </Menu.Item>
-                <Menu.Item key="logout">
+                <Menu.Item key='logout'>
                   <Link
-                    style={{ color: "#1890ff" }}
-                    to="/"
+                    style={{ color: '#1890ff' }}
+                    to='/'
                     onClick={logoutHandler}
                   >
                     Logout
