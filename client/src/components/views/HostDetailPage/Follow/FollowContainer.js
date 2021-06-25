22e85d45824d21bb945bbcbebff70fc2b9bffd8a
JuYoung <ken44929@gmail.com>
2021-06-25 13:51:15 +0900

    prettier 룰 변경에 따른 single quote 적용

--- client/src/components/views/HostDetailPage/Follow/FollowContainer.js
+++ client/src/components/views/HostDetailPage/Follow/FollowContainer.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { LoadingOutlined } from "@ant-design/icons";
-import Axios from "axios";
-import FollowPresenter from "./FollowPresenter";
+import React, { useState, useEffect } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import Axios from 'axios';
+import FollowPresenter from './FollowPresenter';
 
 function FollowContainer({ userFrom, detail, url }) {
   const [loading, setloading] = useState(true);
@@ -16,45 +16,46 @@ function FollowContainer({ userFrom, detail, url }) {
 
   const onClickFollow = () => {
     if (Followed) {
-      Axios.post("/api/follow/removeFollow", variables).then((response) => {
+      Axios.post('/api/follow/removeFollow', variables).then((response) => {
         if (response.data.success) {
           setFollowNumber(FollowNumber - 1);
           setFollowed(!Followed);
         } else {
-          alert("Follow 리스트에서 지우는걸 실패했습니다.");
+          alert('Follow 리스트에서 지우는걸 실패했습니다.');
         }
       });
     } else {
-      Axios.post("/api/follow/addFollow", variables).then((response) => {
+      Axios.post('/api/follow/addFollow', variables).then((response) => {
         if (response.data.success) {
           setFollowNumber(FollowNumber + 1);
           setFollowed(!Followed);
         } else {
-          alert("Follow 리스트에서 추가하는데에 실패했습니다.");
+          alert('Follow 리스트에서 추가하는데에 실패했습니다.');
         }
       });
     }
   };
 
   useEffect(() => {
-    Axios.post("/api/follow/follow-number", variables).then((response) => {
+    Axios.post('/api/follow/follow-number', variables).then((response) => {
       setFollowNumber(response.data.followNumber);
       setloading(false);
     });
 
-    Axios.post("/api/follow/followed", variables).then((response) => {
+    Axios.post('/api/follow/followed', variables).then((response) => {
       if (response.data.success) {
         setFollowed(response.data.followed);
         setloading(false);
-      } else alert("정보를 가져오는 데 실패했습니다.");
+      } else alert('정보를 가져오는 데 실패했습니다.');
     });
+    setloading(false);
   }, []);
 
   return (
     <>
       {loading && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <LoadingOutlined style={{ fontSize: "10px" }} />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <LoadingOutlined style={{ fontSize: '10px' }} />
         </div>
       )}
 
