d705a036f62330e875e2e64fc1d540c62ccab0c9
JuYoung <ken44929@gmail.com>
2021-07-03 13:45:57 +0900

    prettier 새 규칙 적용, Cooking 문구 변경

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, Typography } from "antd";
+import React from 'react';
+import { Alert, Typography } from 'antd';
 import {
   LandingPageContainer,
   TopBoxPanel,
@@ -16,25 +16,25 @@ import {
   MissionInfoBox,
   MissionInfoImg,
   MissionInfoParagraph,
-} from "./LandingStyle";
-import { HeartFilled } from "@ant-design/icons";
-import ramen from "../../Images/ramen.svg";
-import man_chef from "../../Images/man_chef.svg";
-import kitchen from "../../Images/kitchen.svg";
-import cutlery from "../../Images/cutlery.svg";
-import ChefOutlined from "../../Images/ChefOutlined.svg";
-import restaurant from "../../Images/restaurant.svg";
-import { EVENTS_CLIENT, KITCHNES_CLIENT, HOST_CLIENT } from "../../Config";
+} from './LandingStyle';
+import { HeartFilled } from '@ant-design/icons';
+import ramen from '../../Images/ramen.svg';
+import man_chef from '../../Images/man_chef.svg';
+import kitchen from '../../Images/kitchen.svg';
+import cutlery from '../../Images/cutlery.svg';
+import ChefOutlined from '../../Images/ChefOutlined.svg';
+import restaurant from '../../Images/restaurant.svg';
+import { EVENTS_CLIENT, KITCHNES_CLIENT, HOST_CLIENT } from '../../Config';
 
 const { Title } = Typography;
 
 function LandingPage() {
   return (
     <>
-      <div style={{ padding: "0px" }}>
+      <div style={{ padding: '0px' }}>
         <Alert
-          type="success"
-          message="Love cooking and want to host your own pop-up? Contact us on ken44929@gmail.com! "
+          type='success'
+          message='Love cooking and want to host your own pop-up? Contact us on ken44929@gmail.com! '
           icon={<HeartFilled />}
           showIcon
           closable
@@ -43,32 +43,30 @@ function LandingPage() {
           <TopBoxPanels>
             <TopBoxPanel
               style={{
-                backgroundColor: "rgb(255, 236, 184)",
-                animationDelay: "0.1s",
+                backgroundColor: 'rgb(255, 236, 184)',
+                animationDelay: '0.1s',
               }}
             >
-              <TopBoxContent style={{ animationDelay: "0.8s" }}>
-                <TopBoxImg src={ramen} alt="street_food" />
+              <TopBoxContent style={{ animationDelay: '0.8s' }}>
+                <TopBoxImg src={ramen} alt='street_food' />
                 <TopButton to={EVENTS_CLIENT}>Find Events</TopButton>
               </TopBoxContent>
             </TopBoxPanel>
-            <TopBoxPanel
-              style={{ backgroundColor: "skyblue", animationDelay: "0.3s" }}
-            >
-              <TopBoxContent style={{ animationDelay: "1s" }}>
-                <TopBoxImg src={man_chef} alt="street_food" />
+            <TopBoxPanel style={{ backgroundColor: 'skyblue', animationDelay: '0.3s' }}>
+              <TopBoxContent style={{ animationDelay: '1s' }}>
+                <TopBoxImg src={man_chef} alt='street_food' />
                 <TopButton to={HOST_CLIENT}>See Our Hosts</TopButton>
               </TopBoxContent>
             </TopBoxPanel>
             <TopBoxPanel
               style={{
-                backgroundColor: "rgb(183, 222, 210)",
-                animationDelay: "0.5s",
+                backgroundColor: 'rgb(183, 222, 210)',
+                animationDelay: '0.5s',
               }}
             >
-              <TopBoxContent style={{ animationDelay: "1.2s" }}>
-                {" "}
-                <TopBoxImg src={kitchen} alt="street_food" />
+              <TopBoxContent style={{ animationDelay: '1.2s' }}>
+                {' '}
+                <TopBoxImg src={kitchen} alt='street_food' />
                 <TopButton to={KITCHNES_CLIENT}>Provide Kitchens</TopButton>
               </TopBoxContent>
             </TopBoxPanel>
@@ -76,15 +74,12 @@ function LandingPage() {
         </LandingPageContainer>
         <MiddleBox>
           <VisionTextBox>
-            <Title style={{ color: "#e7e7e7", fontWeight: "700" }}>
-              Our Story
-            </Title>
+            <Title style={{ color: '#e7e7e7', fontWeight: '700' }}>Our Story</Title>
             <VisionParagraph>
-              (Cooking)은 요리를 좋아하는, 즐기는 사람들을 위한 요리 공유
-              플랫폼입니다. (Cooking)은 자신의 요리를 대접하기를 좋아하던 한
-              사람의 아이디어에서 성장했습니다. Cooking은 가정 요리사에서 전문
-              요리사에 이르기까지 모든 사람을 지원하고 좋은 음식을 찾는
-              사람들에게 최고의 요리사와 공간을 제공합니다.
+              Cooking은 요리를 좋아하는, 즐기는 사람들을 위한 요리 공유 플랫폼입니다. Cooking은
+              자신의 요리를 대접하기를 좋아하던 한 사람의 아이디어에서 성장했습니다. Cooking은 가정
+              요리사에서 전문 요리사에 이르기까지 모든 사람을 지원하고 좋은 음식을 찾는 사람들에게
+              최고의 요리사와 공간을 제공합니다.
             </VisionParagraph>
             <MiddleButton to={EVENTS_CLIENT}>요리 찾아보기</MiddleButton>
           </VisionTextBox>
@@ -93,8 +88,8 @@ function LandingPage() {
           <div>
             <Title
               style={{
-                fontWeight: "700",
-                textAlign: "center",
+                fontWeight: '700',
+                textAlign: 'center',
               }}
             >
               Our Mission
@@ -103,7 +98,7 @@ function LandingPage() {
           <MissionFlexContainer>
             <MissionInfoBox>
               <MissionInfoImg src={cutlery} />
-              <Title level={4} style={{ marginTop: "10px" }}>
+              <Title level={4} style={{ marginTop: '10px' }}>
                 좋은 요리를 위해
               </Title>
               <MissionInfoParagraph>
@@ -112,7 +107,7 @@ function LandingPage() {
             </MissionInfoBox>
             <MissionInfoBox>
               <MissionInfoImg src={ChefOutlined} />
-              <Title level={4} style={{ marginTop: "10px" }}>
+              <Title level={4} style={{ marginTop: '10px' }}>
                 요리사를 위해
               </Title>
               <MissionInfoParagraph>
@@ -121,7 +116,7 @@ function LandingPage() {
             </MissionInfoBox>
             <MissionInfoBox>
               <MissionInfoImg src={restaurant} />
-              <Title level={4} style={{ marginTop: "10px" }}>
+              <Title level={4} style={{ marginTop: '10px' }}>
                 음식점을 위해
               </Title>
               <MissionInfoParagraph>
