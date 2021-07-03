a445cd36a3a493fee2b411006a9a48529df34d1a
JuYoung <ken44929@gmail.com>
2021-07-03 11:11:10 +0900

    prettier 룰 변경에 따른 새 규칙 적용

--- client/src/components/views/BecomeAHostPage/BecomeAHostPresenter.js
+++ client/src/components/views/BecomeAHostPage/BecomeAHostPresenter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Result,
   Alert,
@@ -11,8 +11,8 @@ import {
   Button,
   Divider,
   Steps,
-} from "antd";
-import { BreadCrumbImg, MiddleBox, BottomBox } from "./BecomeAHostStyle";
+} from 'antd';
+import { BreadCrumbImg, MiddleBox, BottomBox } from './BecomeAHostStyle';
 
 const { Option } = Select;
 const { Step } = Steps;
@@ -27,10 +27,10 @@ function BecomeAHostPresenter({
 }) {
   return (
     <>
-      <Row type="flex">
+      <Row type='flex'>
         {/*------------------- Image -----------------------*/}
         <BreadCrumbImg>
-          <Title level={1} style={{ color: "white" }}>
+          <Title level={1} style={{ color: 'white' }}>
             Become a Host
           </Title>
         </BreadCrumbImg>
@@ -39,52 +39,34 @@ function BecomeAHostPresenter({
         {/*------------------- Inputs -----------------------*/}
 
         {ShowSuccess ? (
-          <Col
-            lg={24}
-            md={24}
-            xs={24}
-            style={{ display: "flex", justifyContent: "center" }}
-          >
+          <Col lg={24} md={24} xs={24} style={{ display: 'flex', justifyContent: 'center' }}>
             <MiddleBox>
               <Result
-                status="success"
-                title="Successfully send the information"
-                subTitle="It will take a few days to answer. Thank you for your decision!"
+                status='success'
+                title='Successfully send the information'
+                subTitle='It will take a few days to answer. Thank you for your decision!'
               />
             </MiddleBox>
           </Col>
         ) : (
-          <Col
-            lg={24}
-            md={24}
-            xs={24}
-            style={{ display: "flex", justifyContent: "center" }}
-          >
+          <Col lg={24} md={24} xs={24} style={{ display: 'flex', justifyContent: 'center' }}>
             <MiddleBox>
               <Alert
-                message="Invite Only"
-                description="Currently, we are operating on invite-only. Feel free to leave your contact information to be contacted in the future."
-                type="info"
+                message='Invite Only'
+                description='Currently, we are operating on invite-only. Feel free to leave your contact information to be contacted in the future.'
+                type='info'
                 showIcon
               />
-              <Title level={4} style={{ color: "gray", paddingTop: "10px" }}>
+              <Title level={4} style={{ color: 'gray', paddingTop: '10px' }}>
                 Tell us a little about yourself
               </Title>
               <Form onSubmit={onSubmit}>
                 {/* Name */}
-                <Input
-                  placeholder="Name"
-                  size="large"
-                  onChange={onNameChange}
-                />
+                <Input placeholder='Name' size='large' onChange={onNameChange} />
                 <br />
                 <br />
                 {/* Email */}
-                <Input
-                  placeholder="Email"
-                  size="large"
-                  onChange={onEmailChange}
-                />
+                <Input placeholder='Email' size='large' onChange={onEmailChange} />
                 <br />
                 <br />
 
@@ -92,24 +74,19 @@ function BecomeAHostPresenter({
                 <Select
                   labelInValue
                   noStyle
-                  size="large"
-                  placeholder="Previous industrial kitchen experience"
+                  size='large'
+                  placeholder='Previous industrial kitchen experience'
                   onChange={onKitchen_ExChange}
                 >
-                  <Option value="None">None</Option>
-                  <Option value="Less than a year">Less than a year</Option>
-                  <Option value="More than a year">More than a year</Option>
+                  <Option value='None'>None</Option>
+                  <Option value='Less than a year'>Less than a year</Option>
+                  <Option value='More than a year'>More than a year</Option>
                 </Select>
                 <br />
                 <br />
 
                 {/* Submit Button */}
-                <Button
-                  onClick={onSubmit}
-                  size="large"
-                  type="primary"
-                  style={{ width: "100%" }}
-                >
+                <Button onClick={onSubmit} size='large' type='primary' style={{ width: '100%' }}>
                   I'm intersted
                 </Button>
               </Form>
@@ -125,30 +102,29 @@ function BecomeAHostPresenter({
         <Title level={2}>Why Host an Event?</Title>
 
         <Paragraph>
-          Grow your brand as a chef, test out your new recipes, and share your
-          lovely food with the community. You’re in full control of your
-          availability, prices, and rules.
+          Grow your brand as a chef, test out your new recipes, and share your lovely food with the
+          community. You’re in full control of your availability, prices, and rules.
         </Paragraph>
 
         <Divider />
 
         {/* Second Bottom-Page */}
         <Title level={2}>Hosting in 3 Steps</Title>
-        <Steps direction="vertical">
+        <Steps direction='vertical'>
           <Step
-            status="process"
-            title="Apply"
-            description="Apply to become a host. We will perform basic identity verficiation and background checks."
+            status='process'
+            title='Apply'
+            description='Apply to become a host. We will perform basic identity verficiation and background checks.'
           />
           <Step
-            status="process"
-            title="Plan Your Event"
-            description="Bring your own kitchen or look for local kitchens who would love to host you."
+            status='process'
+            title='Plan Your Event'
+            description='Bring your own kitchen or look for local kitchens who would love to host you.'
           />
           <Step
-            status="process"
-            title="Cook"
-            description="We handle all the logistics before, during, and after the event. No need to worry about invoices, payments, and ticketing. Connect with your customers in unique ways. Grow your following and automatically update them of your future events."
+            status='process'
+            title='Cook'
+            description='We handle all the logistics before, during, and after the event. No need to worry about invoices, payments, and ticketing. Connect with your customers in unique ways. Grow your following and automatically update them of your future events.'
           />
         </Steps>
 
@@ -156,22 +132,22 @@ function BecomeAHostPresenter({
         {/* Third Bottom-Page */}
 
         <Title level={2}>Payments made simple</Title>
-        <Steps direction="vertical">
+        <Steps direction='vertical'>
           <Step
-            status="finish"
-            title="Low fees"
-            description="There’s no cost to sign up and host your events. Souskasa generally charges lower than the event ticketing industry standard."
+            status='finish'
+            title='Low fees'
+            description='There’s no cost to sign up and host your events. Cooking generally charges lower than the event ticketing industry standard.'
           />
           <Step
-            status="finish"
-            title="Plan Your Event"
-            description="Know how much revenue you will make before your event even happens (minus tips)."
+            status='finish'
+            title='Plan Your Event'
+            description='Know how much revenue you will make before your event even happens (minus tips).'
           />
           <Step
-            status="finish"
-            title="Get paid quickly"
-            description="
-Once the event ends, the balance will be available in your dashboard to payout by direct deposit."
+            status='finish'
+            title='Get paid quickly'
+            description='
+Once the event ends, the balance will be available in your dashboard to payout by direct deposit.'
           />
         </Steps>
       </BottomBox>
