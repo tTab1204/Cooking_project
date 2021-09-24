3840a7d21b684c44622f99b6c01bd57aada6e564
JuYoung <ken44929@gmail.com>
2021-09-24 21:47:40 +0900

    Feat: [#33] 색상을 모아놓은 Theme.js 파일 추가

---
+++ client/src/styles/Theme.js
@@ -0,0 +1,15 @@
+export const Theme = {
+  color: {
+    primary_color: '#1890ff',
+    primary_color2: '#854fee',
+    primary_color3: '#4458dc',
+    black_color: '#141313',
+    text_color: '#777777',
+    border_color: '#201f1f17',
+    background_color:
+      'to bottom rgba(0, 0, 0, 0) 39%, rgba(0, 0, 0, 0) 41%, rgba(0, 0, 0, 0.65) 100%',
+    gradient_color: 'linear-gradient(90deg, #4458dc 0%, #854fee 100%)',
+    shadow_color: '0px 10px 30px rgba(118, 85, 225, 0.3)',
+    box_shadow: '0px 10px 30px rgba(57, 56, 61, 0.205)',
+  },
+};
