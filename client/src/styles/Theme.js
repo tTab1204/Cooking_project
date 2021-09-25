a1a48b8b909f586df68cf937b9e6409ff380f9eb
JuYoung <ken44929@gmail.com>
2021-09-25 12:07:42 +0900

    Feat: [#33] 색상을 모아놓은 Theme.js 파일 생성

--- client/src/styles/Theme.js
+++ client/src/styles/Theme.js
@@ -1,15 +1,13 @@
-export const Theme = {
-  color: {
-    primary_color: '#1890ff',
-    primary_color2: '#854fee',
-    primary_color3: '#4458dc',
-    black_color: '#141313',
-    text_color: '#777777',
-    border_color: '#201f1f17',
-    background_color:
-      'to bottom rgba(0, 0, 0, 0) 39%, rgba(0, 0, 0, 0) 41%, rgba(0, 0, 0, 0.65) 100%',
-    gradient_color: 'linear-gradient(90deg, #4458dc 0%, #854fee 100%)',
-    shadow_color: '0px 10px 30px rgba(118, 85, 225, 0.3)',
-    box_shadow: '0px 10px 30px rgba(57, 56, 61, 0.205)',
-  },
+export const color = {
+  primary: '#1890ff',
+  primary_2: '#854fee',
+  primary_3: '#4458dc',
+  black: '#1c1c1c',
+  text: '#777777',
+  border: '#201f1f17',
+  background_img:
+    'linear-gradient(to bottom, rgba(0, 0, 0, 0) 39%, rgba(0, 0, 0, 0) 41%, rgba(0, 0, 0, 0.65) 100%)',
+  gradient: 'linear-gradient(90deg, #4458dc 0%, #854fee 100%)',
+  shadow: '0px 10px 30px rgba(118, 85, 225, 0.3)',
+  box_shadow: '0px 10px 30px rgba(57, 56, 61, 0.205)',
 };
