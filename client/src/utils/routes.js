123d9274f9abea3e57285eb9ca4d2d1068314d0c
JuYoung <ken44929@gmail.com>
2021-09-25 13:50:14 +0900

    Feat: [#38] utils 폴더 하위의 routes.js 파일 생성

---
+++ client/src/utils/routes.js
@@ -0,0 +1,25 @@
+export const ROUTES = {
+  MAIN: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  HOST: {
+    MAIN: '/hosts',
+    DETAIL: '/hosts/:hostsId',
+  },
+  BECOME_A_HOST: '/become-a-host',
+  LIST_YOUR_KITCHEN: '/list-your-kitchen',
+  KITCHENS: {
+    MAIN: '/kitchens',
+    DETAIL: '/kitchens/:kitchensId',
+  },
+  USER_PROFILE: '/users/:userName',
+  MY_TICKETS: '/my-tickets',
+  PAYMENT: '/payment',
+  EVENTS: {
+    MAIN: '/events',
+    DETAIL: '/events/:eventsId',
+  },
+  UPLOAD_KITCHEN: '/upload-kitchen',
+  UPLOAD_HOST: '/upload-host',
+  UPLOAD_EVENT: '/upload-event',
+};
