5f2d9c2585399d30d9fd62e31e528795fa0ee736
JuYoung <ken44929@gmail.com>
2021-09-27 20:09:58 +0900

    Style: MiddleBox styled-component 생성

--- client/src/styles/GlobayStyle.js
+++ client/src/styles/GlobayStyle.js
@@ -57,6 +57,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 `;
 
+// App.js
 export const AffixStyle = {
   height: 40,
   width: 40,
@@ -78,4 +79,15 @@ export const MainBox = styled.div`
   @media screen and (max-width: 960px) {
     padding: 0;
   }
+`;
+
+export const MiddleBox = styled.div`
+  max-width: 600px;
+  padding-left: 24px;
+  padding-right: 24px;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
 `;
