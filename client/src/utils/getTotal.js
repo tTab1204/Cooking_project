e5b5e97b33f65c9c2c749a7b269fd8af23c0e6e3
JuYoung <ken44929@gmail.com>
2021-10-01 17:40:02 +0900

    Refactor: 카트 상품의 총액을 계산하는 함수 추가

--- client/src/utils/getTotal.js
+++ client/src/utils/getTotal.js
@@ -1,7 +1,15 @@
 export const getTotal = cart => {
   let total = 0;
-
-  cart.map(item => (total += item.quantity));
+  cart && cart.map(item => (total += item.quantity));
 
   return total;
+};
+
+export const getTotalPrice = (cartDetail, setTotal) => {
+  let totalPrice = 0;
+
+  cartDetail.map(item => {
+    totalPrice += parseInt(item.price, 10) * item.quantity;
+  });
+  setTotal(totalPrice);
 };
