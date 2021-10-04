e2ce17df7ef4070aee3392da49a2cef81c887179
JuYoung <ken44929@gmail.com>
2021-10-04 14:41:20 +0900

    Refactor: kitchens data를 가져오는 API 추가

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,3 +2,6 @@
 export const SHOW_EVENTS = '/api/events/show-events';
 export const SHOW_EVENT_DETAIL = eventId =>
   `/api/events/events_by_id?id=${eventId}&type=single`;
+
+/* kitchens */
+export const SHOW_KITCHENS = '/api/kitchens/show-kitchens';
