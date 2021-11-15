2343667336ccc0453a3349ed789ea1f76e74e472
JuYoung <ken44929@gmail.com>
2021-11-15 16:12:19 +0900

    Feat: multer-s3, aws-sdk 설치, gitignore 업데이트

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,36 +3,56 @@ const router = express.Router();
 const { auth } = require('../middleware/auth');
 const { Event } = require('../models/Event');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
-// ---------------- multer----------------------//
-let storage = multer.diskStorage({
-  // destination: 어디에 파일을 저장할 지
-  destination: (req, file, cb) => {
-    // destination: uploads폴더가 된다. uploads 폴더에 사진 저장.
-    cb(null, 'uploads/');
-  },
-  // 파일 이름
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
-  },
-  // 파일 형식은 png 또는 jpg 등, 즉 사진만 가능
-  fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    if (ext !== '.png' || ext !== '.jpg') {
-      return cb(res.status(400).end('only png, jpg is allowed'), false);
-    }
-    cb(null, true);
-  },
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northease-2',
 });
 
-const upload = multer({ storage: storage }).single('file');
+// ---------------- multer----------------------//
+// let storage = multer.diskStorage({
+//   // destination: 어디에 파일을 저장할 지
+//   destination: (req, file, cb) => {
+//     // destination: uploads폴더가 된다. uploads 폴더에 사진 저장.
+//     cb(null, 'uploads/');
+//   },
+//   // 파일 이름
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}_${file.originalname}`);
+//   },
+//   // 파일 형식은 png 또는 jpg 등, 즉 사진만 가능
+//   fileFilter: (req, file, cb) => {
+//     const ext = path.extname(file.originalname);
+//     if (ext !== '.png' || ext !== '.jpg') {
+//       return cb(res.status(400).end('only png, jpg is allowed'), false);
+//     }
+//     cb(null, true);
+//   },
+// });
+
+// const upload = multer({ storage: storage }).single('file');
+
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'cooking-aws-s3',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
+      // original이란 폴더를 만들고 그 곳에 넣는 것
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
 
 router.post('/upload-image', auth, (req, res) => {
   // 노드서버에 파일을 저장하기 위한 dependency를 설치한다.(multer)
   // npm install multer --save
 
   //upload 변수를 불러온다.
-  upload(req, res, (err) => {
+  upload(req, res, err => {
     if (err) {
       return res.json({ success: false, err });
     }
@@ -42,7 +62,7 @@ router.post('/upload-image', auth, (req, res) => {
     return res.json({
       success: true,
       // 클라이언트(프론트)에 image, fileName 정보를 보내준다.
-      image: res.req.file.path,
+      image: res.req.file.location,
       fileName: res.req.file.filename,
     });
   });
@@ -90,7 +110,7 @@ router.get('/events_by_id', auth, (req, res) => {
   let eventIds = req.query.id;
   if (type === 'array') {
     let ids = req.query.id.split(',');
-    eventIds = ids.map((item) => {
+    eventIds = ids.map(item => {
       return item;
     });
   }
