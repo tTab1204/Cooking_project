2393c56950d3d8a5ccd6147fa17f4ec93a8f8637
JuYoung <ken44929@gmail.com>
2021-11-15 16:25:21 +0900

    Fix: aws s3 bucket region 수정

--- server/routes/events.js
+++ server/routes/events.js
@@ -9,7 +9,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: 'ap-northease-2',
+  region: 'ap-northeast-2',
 });
 
 // ---------------- multer----------------------//
