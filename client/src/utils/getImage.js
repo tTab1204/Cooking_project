33999a2c948a5bb712721a5136257fca674ee5ee
JuYoung <ken44929@gmail.com>
2021-11-15 17:04:02 +0900

    Chore: Test deploy

--- client/src/utils/getImage.js
+++ client/src/utils/getImage.js
@@ -2,5 +2,6 @@ import { LOCAL_SERVER } from './config';
 import { production } from './constants';
 
 export const getImage = imgName => {
+  console.log('production: ', production);
   return production ? imgName : `${LOCAL_SERVER}${imgName}`;
 };
